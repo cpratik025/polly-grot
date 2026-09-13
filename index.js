3930df6bb0b2aa42983ece9952656bd6940e781e
Pratik Chorsiya <cpratik025>
2026-09-13 03:42:14 -0400

    Language Translation app

---
+++ index.js
@@ -0,0 +1,54 @@
+import { marked } from "marked";
+import DOMPurify from "dompurify";
+import { autoResizeTextarea, setLoading } from "./utils.js";
+
+const langSelect=document.getElementById('language-select')
+const translateBtn=document.getElementById('translate-btn')
+const translateText=document.getElementById('input-text')
+const btnContainer=document.getElementById('btn-container')
+const startChatbtn=document.getElementById('start-chat')
+
+translateBtn.addEventListener('click',textToTranslate)
+
+async function textToTranslate(e){
+    e.preventDefault()
+    const langToTranslate=document.querySelector('input[name="language"]:checked')
+    const userPrompt=`Translate the following text into ${langToTranslate.value}: "${translateText.value}"`
+    console.log(userPrompt)
+    const requestBody= await fetch('/api/translate',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify({userPrompt})
+    })
+    console.log(requestBody)
+    const data=await requestBody.json()
+    if(!requestBody.ok){
+        throw new Error(data.message)
+    }
+    
+    const translatedText=data.translatedText
+    const markedhtml = marked.parse(translatedText)
+    const safeHTML = DOMPurify.sanitize(markedhtml)
+    let html=`<h1 class="input-title">Translated Text</h1>
+    <textarea id="input-text" placeholder="How are you?" readonly>${translatedText}</textarea>
+    `
+    langSelect.innerHTML=html
+
+    let btnHtml=`
+                <button class="btn" id="start-over">Start Over</button>
+                <button class="btn" id="start-chat">Start Chat</button>
+            `
+    btnContainer.innerHTML=btnHtml
+
+    document.getElementById('start-over').addEventListener('click', () => {
+        window.location.href = "./index.html";
+    })
+    document.getElementById('start-chat').addEventListener('click', () => {
+        window.location.href = "./chat.html";
+    })}
+
+    startChatbtn.addEventListener('click', () => {
+        window.location.href = "./chat.html";
+    })
+
+
---
+++ start.js
@@ -0,0 +1,38 @@
+import { marked } from "marked";
+import DOMPurify from "dompurify";
+import { autoResizeTextarea, setLoading } from "./utils.js";
+
+
+let sendBtn= document.getElementById('send-btn')
+let userInput= document.getElementById('user-input')
+let chatContainer= document.getElementById('chat-messages')
+sendBtn.addEventListener('click',chatMessage)
+
+async function chatMessage(e){
+    e.preventDefault()
+    const langToTranslate=document.querySelector('input[name="language"]:checked')
+    console.log(langToTranslate)
+    const userPrompt=`Translate the following text into ${langToTranslate.value}: "${userInput.value}"`
+    let addUserHTML=`<div class="user-message">
+                    <p>${userInput.value}</p>
+                </div>`
+    chatContainer.innerHTML+=addUserHTML
+    const requestBody= await fetch('/api/translate',{
+        method: 'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify({userPrompt})
+    })
+    const data=await requestBody.json()
+    
+    if(!requestBody.ok){
+        throw new Error(data.message)
+    }
+    
+    const translatedText=data.translatedText
+    const html = marked.parse(translatedText);
+    const safeHTML = DOMPurify.sanitize(html)
+    let responseHTML=`<div class="system-message">
+                    <p>${safeHTML}</p>
+                </div>`
+    chatContainer.innerHTML+=responseHTML
+}
